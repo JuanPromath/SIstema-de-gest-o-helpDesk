27ca14904c6e1a49ce4393461703ba77723695a4
JavaNoobGuy <116377217+javaNoobGuy@users.noreply.github.com>
2025-11-17 16:00:41 -0300

    fdfsdfds

--- projetoUmc/create/createChamadoAdm.php
+++ projetoUmc/create/createChamadoAdm.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Criação de Chamado</title>
-  <link rel="stylesheet" href="styleChamado.css">
+  <link rel="stylesheet" href="styleChamado2.css">
 </head>
 <body>
 
--- projetoUmc/listar.php
+++ projetoUmc/listar.php
@@ -2,6 +2,4 @@
 require_once 'conexao.php';
 header('Content-Type: application/json');
 $all = teste('cargo');
-
-
 ?>
